7fd916005aca84fcd49f61422b57c9d8c192631c
Naj Dhillon <najdhil86@gmail.com>
2019-07-04 16:17:15 -0700

    adding work for hw 2

--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,46 @@
 
+//Total Guesses left
 var totalGuess = 9;
 
+// Wins
+var win = 0;
 
-var showKey = document.write(event.key);
+//Lose
+var lose = 0;
+
+
+//Random choice of computer choice
+
+//Computer's Option
+var cOptions = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+
+// Makes a random index
+var cRandom = Math.floor(Math.random()*cOptions.length)
+
+// Chooses a random letter from cOptions
+var cChoice = cOptions[cRandom];
+
+//Checking random letter
+// console.log(cChoice);
+
+
+
+
+
+//Your guesses so far. This area of the code will display what letter the user is typing
+
+// var userLetter = "";
+var guessKey = document.querySelector(".userInputs");
+
+function userKey(){
+    //this event object available to us
+    //event.key gives you the key pressed
+    var guessKey = document.querySelector(".userInputs");
+    var userLetter = [];
+    guessKey.innerText = event.key;
+}
+
+document.onkeyup = userKey;
+
+//empty array
+//
